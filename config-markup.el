e2d1cbba0d0ef12b5fc1ff5c6a851c7b25f60b31
Alfredo Aguirre <alfredoaguirre@theteam.co.uk>
2011-09-07 15:03:06 +0100

    Removed auto-fill

--- config-markup.el
+++ config-markup.el
@@ -2,6 +2,9 @@
 (autoload 'html-helper-mode "html-helper-mode" "Yay HTML" t)
 (setq auto-mode-alist (cons '("\\.gsp$" . html-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.html$" . html-mode) auto-mode-alist))
+;; disable autofill
+(add-hook 'html-mode-hook 'turn-off-auto-fill)
+
 
 ;; PHP mode
 ;;(autoload 'php-mode "php-mode.el" "Php mode." t)
