96e9f221bf53220994cb966869b5eabeef7411ca
Alfredo Aguirre <aguirre.alfredo07@gmail.com>
2011-09-08 08:06:08 +0100

    High contrast on the theme

--- config-color-themes.el
+++ config-color-themes.el
@@ -58,6 +58,7 @@
 
 (require 'color-theme-solarized)
 (color-theme-solarized-dark)
+(setq solarized-contrast "high")
 (ansi-color-for-comint-mode-on)
 (setq inhibit-startup-message t)
 (set-default 'indicate-empty-lines nil)
