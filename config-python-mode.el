4fe658ac5029d95d6a1d549ee41d7a91b2361c36
Alfredo Aguirre <aguirre.alfredo07@gmail.com>
2011-10-09 19:12:13 +0100

    Using the mighty python-mode.el with way better indentation rules

--- config-python-mode.el
+++ config-python-mode.el
@@ -1,5 +1,8 @@
 ;; python config
-(require 'python)
+;;(require 'python)
+(autoload 'python-mode "python-mode" "Python Mode." t)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
 (when (load "flymake" t)
   (defun flymake-pyflakes-init ()
