2211075d66b95eba7f863c1aab3c6ecf2fccc33d
Alfredo Aguirre <aguirre.alfredo07@gmail.com>
2011-11-22 22:21:22 +0000

    Added magit and vagrant modes

--- config-environment.el
+++ config-environment.el
@@ -78,3 +78,7 @@
 
 (autoload 'puppet-mode "puppet-mode" "Major mode for editing puppet manifests")
 (add-to-list 'auto-mode-alist '("\\.pp$" . puppet-mode))
+
+(require 'magit)
+;; vagrantvile is like ruby
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
--- config-python-mode.el
+++ config-python-mode.el
@@ -4,16 +4,20 @@
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
+;;(setq pyflymakeexec "/usr/local/bin/pyflakespep8.py")
+(setq pyflymakeexec "/usr/local/bin/pycheckers")
+
+;; Pyflakes for python
 (when (load "flymake" t)
-  (defun flymake-pyflakes-init ()
+  (defun flymake-pychecker-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
-               'flymake-create-temp-inplace))
-       (local-file (file-relative-name
-            temp-file
-            (file-name-directory buffer-file-name))))
-      (list "pycheckers"  (list local-file))))
-   (add-to-list 'flymake-allowed-file-name-masks
-             '("\\.py\\'" flymake-pyflakes-init)))
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list pyflymakeexec (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pychecker-init)))
 
 (add-hook 'python-mode-hook 'flymake-mode)
 (load-library "flymake-cursor")
