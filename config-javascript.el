fafdc0cd5ab70f2a93d4723c029ccad8ec3f2460
Alfredo Aguirre <aguirre.alfredo07@gmail.com>
2011-12-31 15:51:15 +0000

    Added coffee script

--- config-javascript.el
+++ config-javascript.el
@@ -19,6 +19,19 @@
 (add-hook 'espresso-mode-hook
 	  (lambda () (flymake-mode t)))
 
+;; coffee-script heh
+
+(add-to-list 'load-path "~/.emacs.d/plugins/coffee-mode")
+(require 'coffee-mode)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+;; idiomatic CoffeeScript uses two spaces
+(defun coffee-custom ()
+  "coffee-mode-hook"
+ (set (make-local-variable 'tab-width) 2))
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
 
 ;; http://closure-compiler.googlecode.com/files/compiler-latest.zip
 
