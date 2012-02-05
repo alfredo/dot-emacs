ff470cf21658c58deed5763814c74634eedaaf6c
Alfredo Aguirre <aguirre.alfredo07@gmail.com>
2012-02-05 22:37:40 +0000

    Default window size

--- config-environment.el
+++ config-environment.el
@@ -97,3 +97,23 @@
 (require 'expand-region)
 (global-set-key (kbd "C-@") 'er/expand-region)
 (delete-selection-mode 1)
+
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+  (progn
+    ;; use 120 char wide window for largeish displays
+    ;; and smaller 80 column windows for smaller displays
+    ;; pick whatever numbers make sense for you
+    (if (> (x-display-pixel-width) 1280)
+           (add-to-list 'default-frame-alist (cons 'width 170))
+           (add-to-list 'default-frame-alist (cons 'width 80)))
+    ;; for the height, subtract a couple hundred pixels
+    ;; from the screen height (for panels, menubars and
+    ;; whatnot), then divide by the height of a char to
+    ;; get the height we want
+    (add-to-list 'default-frame-alist 
+         (cons 'height (/ (- (x-display-pixel-height) 80)
+                             (frame-char-height)))))))
+
+(set-frame-size-according-to-resolution)
