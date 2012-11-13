99bda57fc5116f01f3dec8f4d93314728191a1b0
Alfredo Aguirre <alfredo@madewithbyt.es>
2012-11-13 22:43:10 +0000

    Updated installed packages.

--- config-package.el
+++ config-package.el
@@ -4,7 +4,8 @@
 (setq package-list '(exec-path-from-shell flycheck flymake-easy flymake-cursor python dropdown-list flyspell
 	expand-region smex yasnippet color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized cyberpunk-theme
 	php-mode sass-mode js2-mode flymake-jslint coffee-mode bash-completion mic-paren puppet-mode magit
-	auto-complete ctags fixmee flex-isearch gist goto-chg logito ido-ubiquitous bm))
+	auto-complete ctags fixmee flex-isearch gist goto-chg logito ido-ubiquitous bm browse-kill-ring groovy-mode
+	iedit python-magic redo+ restclient))
 
 ;; TODO: missing rainbow-mode
 
