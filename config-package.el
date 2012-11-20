d99a4b4ed4df38cf5ded8a325d6c7e1b754290f7
Alfredo Aguirre <alfredo@madewithbyt.es>
2012-11-20 22:30:50 +0000

    Removed non used status.

--- config-package.el
+++ config-package.el
@@ -2,7 +2,7 @@
 (require 'melpa)
 
 (setq package-list '(exec-path-from-shell smex yasnippet dropdown-list color-theme-sanityinc-tomorrow
-	color-theme-sanityinc-solarized iy-go-to-char key-chord python flymake-cursor flymake-python-pyflakes 
+	color-theme-sanityinc-solarized iy-go-to-char key-chord python flymake-cursor flymake-python-pyflakes
 	bm restclient flymake-jslint flymake-sass flymake-ruby puppet-mode s revive nyan-mode magit mic-paren
 	rainbow-mode expand-region browse-kill-ring))
 
@@ -16,10 +16,10 @@
 
 (setq url-http-attempt-keepalives nil)
 
-; fetch the list of packages available 
+; fetch the list of packages available
 (when (not package-archive-contents)
   (package-refresh-contents))
- 
+
 ; install the missing packages
 (dolist (package package-list)
   (when (not (package-installed-p package))
