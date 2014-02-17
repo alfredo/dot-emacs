df1d633e9b2d700af866e2e6328b482180de63c9
Alfredo Aguirre <alfredo@madewithbyt.es>
2014-02-17 21:25:58 +0000

    Add js configuration.

--- init.el
+++ init.el
@@ -20,5 +20,24 @@
 (load-library "config-python")
 ;; css setup
 (load-library "config-css")
+;; javascript setup
+(load-library "config-javascript")
 ;; custom setup
 (load-library "config-custom")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t))))
+ '(backup-directory-alist (quote ((".*" . "~/.emacs.d/backups/"))))
+ '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(custom-safe-themes (quote ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(grep-program "ack -H -a --nogroup" t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flymake-errline ((((class color)) (:background "#6c71c4" :foreground "#ffffff"))))
+ '(flymake-warnline ((((class color)) (:background "#0a2832")))))
