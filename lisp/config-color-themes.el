83b7c18ba4e21c78c952494e3354d4f37744a7ae
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-25 00:21:00 +0100

    Remove clean line mode.

--- lisp/config-color-themes.el
+++ lisp/config-color-themes.el
@@ -1,42 +1,6 @@
+(setq sml/no-confirm-load-theme t)
+(setq sml/theme 'dark)
+(sml/setup)
 
-;; (color-theme-tomorrow-night-eighties)
-
-;; (require 'color-theme-sanityinc-solarized)
 (require 'moe-theme)
 (load-theme 'moe-dark t)
-;; (load-theme 'firebelly)
-
-;; (require 'nyan-mode)
-;; (add-hook 'text-mode-hook (lambda () (nyan-mode t)))
-;; (add-hook 'python-mode-hook (lambda () (nyan-mode t)))
-
-(defvar mode-line-cleaner-alist
-  `((auto-complete-mode . " α")
-    (yas-minor-mode . " ✂")
-    (eldoc-mode . "")
-    (abbrev-mode . "")
-    ;; Major modes
-    (lisp-interaction-mode . "i")
-    (hi-lock-mode . "")
-    (python-mode . "Π")
-    (projectile-mode . "")
-    (emacs-lisp-mode . "Ε"))
-  "Alist for `clean-mode-line'.
-
-When you add a new element to the alist, keep in mind that you
-must pass the correct minor/major mode symbol and a string you
-want to use in the modeline *in lieu of* the original.")
-
-(defun clean-mode-line ()
-  (interactive)
-  (loop for cleaner in mode-line-cleaner-alist
-        do (let* ((mode (car cleaner))
-                 (mode-str (cdr cleaner))
-                 (old-mode-str (cdr (assq mode minor-mode-alist))))
-             (when old-mode-str
-                 (setcar old-mode-str mode-str))
-               ;; major mode
-             (when (eq mode major-mode)
-               (setq mode-name mode-str)))))
-
-(add-hook 'after-change-major-mode-hook 'clean-mode-line)
