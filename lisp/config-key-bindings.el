6e8267de7531aba225920e1d1c73ae06429d748b
Alfredo Aguirre <alfredo@madewithbyt.es>
2015-12-26 19:51:22 +0100

    Remove indentation function.
    It was messing up my sweet moves.

--- lisp/config-key-bindings.el
+++ lisp/config-key-bindings.el
@@ -75,31 +75,6 @@ there's a region, all lines that region covers will be duplicated."
 (require 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
 
-(defun prelude-move-beginning-of-line (arg)
-  "Move point back to indentation of beginning of line.
-
-Move point to the first non-whitespace character on this line.
-If point is already there, move to the beginning of the line.
-Effectively toggle between the first non-whitespace character and
-the beginning of the line.
-
-If ARG is not nil or 1, move forward ARG - 1 lines first. If
-point reaches the beginning or end of the buffer, stop there."
-  (interactive "^p")
-  (setq arg (or arg 1))
-
-  ;; Move lines first
-  (when (/= arg 1)
-    (let ((line-move-visual nil))
-      (forward-line (1- arg))))
-
-  (let ((orig-point (point)))
-    (back-to-indentation)
-    (when (= orig-point (point))
-      (move-beginning-of-line 1))))
-
-(global-set-key (kbd "C-a") 'prelude-move-beginning-of-line)
-
 
 (defun kill-default-buffer ()
   "Kill the currently active buffer -- set to C-x k so that users are not asked which buffer they want to kill."
