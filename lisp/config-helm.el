a815a5dec6c756ae5e1c747ebf251a9bbd327526
Alfredo Aguirre <alfredo@madewithbyt.es>
2016-05-31 10:27:06 +0100

    Incorporate `helm` in some default emacs methods.

--- lisp/config-helm.el
+++ lisp/config-helm.el
@@ -16,8 +16,10 @@
 ; make TAB works in terminal
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
 (global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-<tab>") 'helm-mini)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 
 (setq helm-ff-skip-boring-files t)
