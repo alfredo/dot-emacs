5c86fd4510fdb9c7a19abeeb3bf8f1b3553830c7
Alfredo Aguirre <alfredo@madewithbyt.es>
2016-05-31 10:19:13 +0100

    Ignore python pep8 lambda warnings.
    Lambdas are useful.

--- lisp/config-python.el
+++ lisp/config-python.el
@@ -1,7 +1,9 @@
 (setenv "LC_CTYPE" "UTF-8")
 
 (setq flymake-python-pyflakes-executable "flake8")
+(setq flymake-python-pyflakes-extra-arguments '("--ignore=E731"))
 ;; (setq flymake-python-pyflakes-executable "pyflakespep8")
+
 
 (require 'flymake-python-pyflakes)
 (add-hook 'python-mode-hook 'flymake-python-pyflakes-load)
