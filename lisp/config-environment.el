5c895e3f6116e81655bc83298f61baf2f9bc194a
Alfredo Aguirre <alfredo@madewithbyt.es>
2017-03-02 16:22:38 +0000

    Update projectile configuration.

--- lisp/config-environment.el
+++ lisp/config-environment.el
@@ -13,33 +13,7 @@
 (require 'expand-region)
 (global-set-key (kbd "C-c e") 'er/expand-region)
 
-;; Do not show scratch at the begining
-(setq inhibit-startup-message t)
-
-;; Start up message
-(setq initial-scratch-message "")
-
 (setq default-directory "~/")
-
-(defun set-frame-size-according-to-resolution ()
-  (interactive)
-  (if window-system
-  (progn
-    ;; use 120 char wide window for largeish displays
-    ;; and smaller 80 column windows for smaller displays
-    ;; pick whatever numbers make sense for you
-    (if (> (x-display-pixel-width) 1280)
-           (add-to-list 'default-frame-alist (cons 'width 170))
-           (add-to-list 'default-frame-alist (cons 'width 80)))
-    ;; for the height, subtract a couple hundred pixels
-    ;; from the screen height (for panels, menubars and
-    ;; whatnot), then divide by the height of a char to
-    ;; get the height we want
-    (add-to-list 'default-frame-alist
-         (cons 'height (/ (- (x-display-pixel-height) 200)
-                          (frame-char-height)))))))
-
-(set-frame-size-according-to-resolution)
 
 ;; Only use y / n as a valid answer
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -57,14 +31,11 @@
 
 ;; show current function
 (require 'which-func)
-(add-to-list 'which-func-modes '(org-mode python-mode javscript-mode c++-mode))
+(add-to-list 'which-func-modes '(org-mode python-mode javscript-mode))
 (which-func-mode 1)
 
 ;; Treat camel-case words as single words
 (subword-mode 1)
-
-;; Enable puppet mode on .pp files
-(setq auto-mode-alist (cons '("\\.pp$" . puppet-mode) auto-mode-alist))
 
 ;; only use spaces as tab
 (setq-default indent-tabs-mode nil)
@@ -74,7 +45,6 @@
 
 ;; Vagrant files
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
-
 
 (defun copy-file-name-to-clipboard ()
   "Copy the current buffer file name to the clipboard."
@@ -126,7 +96,7 @@ Don't mess with special buffers."
 
 (add-hook 'c-mode-common-hook
     (lambda ()
-      (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode, 'python-mode)
+      (when (derived-mode-p 'python-mode)
   (ggtags-mode 1))))
 
 (require 'volatile-highlights)
@@ -180,3 +150,91 @@ Don't mess with special buffers."
 
 (require 're-builder)
 (setq reb-re-syntax 'string)
+
+;; scroll one line at a time (less "jumpy" than defaults)
+;; one line at a time
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+;; don't accelerate scrolling
+(setq mouse-wheel-progressive-speed nil)
+;; scroll window under mouse
+(setq mouse-wheel-follow-mouse 't)
+;; keyboard scroll one line at a time
+(setq scroll-step 1)
+
+
+
+(defun get-parent-dir (directory)
+  ;; Returns the parent directory of the path given
+  (directory-file-name (file-name-directory directory))
+  )
+
+(defun has-test-dir (directory)
+  ;; Test if directory has a `tests` directory:
+  (file-accessible-directory-p (concat directory "/tests"))
+  )
+
+(defun is-end-leaf-dir (directory)
+  ;; Root directory or projectile root:
+  (or
+   (equal directory "/")
+   (equal directory projectile-project-root)
+   (equal directory (getenv "HOME"))
+   )
+  )
+
+(defun replace-in-string (what with in)
+  ;; String replacement
+  (replace-regexp-in-string (regexp-quote what) with in nil 'literal))
+
+
+(defun get-test-file-name (current-dir buffer-file-name)
+  ;; Extracts the file test from the path and buffer name
+  (setq test-file-name
+        (replace-in-string current-dir "" buffer-file-name))
+  (replace-regexp-in-string "^/[[:ascii:]]+?/" "" test-file-name nil 'literal)
+  )
+
+(defun create-test-file (test-file-name)
+  ;; Creates the given test-file-name and its containing dir:
+  (setq test-dir (file-name-directory test-file-name))
+  (setq init-file (concat test-dir "__init__.py"))
+  (if (not (file-exists-p init-file))
+      (
+       (make-directory test-dir)
+       (write-file init-file)
+       )
+    )
+  (find-file test-file-name)
+  )
+
+(defun get-create-test-file (current-dir buffer-file-name)
+  ;; Creates or opens the test file:
+  (setq test-file-name
+        (concat current-dir "/tests/"
+                (get-test-file-name current-dir buffer-file-name)))
+  (if (file-exists-p test-file-name)
+      (find-file test-file-name)
+    (create-test-file test-file-name))
+  ;; (message (concat current-dir "/tests" test-file-name))
+  )
+
+(defun open-test-file ()
+  "Open or creates the test file for current buffer"
+  ;; TODO incorporate this only for python-mode
+  (interactive)
+  ;; Current directory:
+  (setq current-dir (file-name-directory buffer-file-name))
+  (while (or (not (has-test-dir current-dir)) (is-end-leaf-dir current-dir))
+    ;; Walk one directory up:
+    (setq current-dir (get-parent-dir current-dir))
+    )
+  (if (has-test-dir current-dir)
+      (get-create-test-file current-dir buffer-file-name) ;; then
+    (message "No `tests/` directory found.")              ;; else
+    )
+  )
+
+(global-set-key (kbd "C-c t") 'open-test-file)
+(global-set-key (kbd "C-c c") 'comment-or-uncomment-region)
+
+(setq undo-tree-visualizer-timestamps t)
--- lisp/config-projectile.el
+++ lisp/config-projectile.el
@@ -1,86 +1,40 @@
-(setq projectile-indexing-method 'native)
+;; Custom configuration of projectile
+(setq projectile-indexing-method 'alien)
 (setq projectile-remember-window-configs t)
 (setq projectile-enable-caching t)
 (setq projectile-completion-system 'helm)
 (setq projectile-globally-ignored-file-suffixes
       '(
-        "pyc"
+        ".pyc"
+        ".DS_Store"
+        ".cache"
+        ".min.js"
+        ".dist-info"
+        ".so"
+        ".ttf"
+        ".jpg"
+        ".gif"
+        ".mpg"
+        ".mov"
+        ".zip"
+        ".dump"
+        ))
+(setq projectile-globally-ignored-directories
+      '(
+        ".idea"
+        ".ensime_cache"
+        ".eunit"
+        ".git"
+        ".hg"
+        ".fslckout"
+        "_FOSSIL_"
+        ".bzr"
+        "_darcs"
+        ".tox"
+        ".svn"
+        ".stack-work"
+        "migrations"
         ))
 
 (projectile-global-mode)
 (helm-projectile-on)
-
-
-(defun get-parent-dir (directory)
-  ;; Returns the parent directory of the path given
-  (directory-file-name (file-name-directory directory))
-  )
-
-(defun has-test-dir (directory)
-  ;; Test if directory has a `tests` directory:
-  (file-accessible-directory-p (concat directory "/tests"))
-  )
-
-(defun is-end-leaf-dir (directory)
-  ;; Root directory or projectile root:
-  (or
-   (equal directory "/")
-   (equal directory projectile-project-root)
-   (equal directory (getenv "HOME"))
-   )
-  )
-
-(defun replace-in-string (what with in)
-  ;; String replacement
-  (replace-regexp-in-string (regexp-quote what) with in nil 'literal))
-
-
-(defun get-test-file-name (current-dir buffer-file-name)
-  ;; Extracts the file test from the path and buffer name
-  (setq test-file-name
-        (replace-in-string current-dir "" buffer-file-name))
-  (replace-regexp-in-string "^/[[:ascii:]]+?/" "" test-file-name nil 'literal)
-  )
-
-(defun create-test-file (test-file-name)
-  ;; Creates the given test-file-name and its containing dir:
-  (setq test-dir (file-name-directory test-file-name))
-  (setq init-file (concat test-dir "__init__.py"))
-  (if (not (file-exists-p init-file))
-      (
-       (make-directory test-dir)
-       (write-file init-file)
-       )
-    )
-  (find-file test-file-name)
-  )
-
-(defun get-create-test-file (current-dir buffer-file-name)
-  ;; Creates or opens the test file:
-  (setq test-file-name
-        (concat current-dir "/tests/"
-                (get-test-file-name current-dir buffer-file-name)))
-  (if (file-exists-p test-file-name)
-      (find-file test-file-name)
-    (create-test-file test-file-name))
-  ;; (message (concat current-dir "/tests" test-file-name))
-  )
-
-(defun open-test-file ()
-  "Open or creates the test file for current buffer"
-  ;; TODO incorporate this only for python-mode
-  (interactive)
-  ;; Current directory:
-  (setq current-dir (file-name-directory buffer-file-name))
-  (while (or (not (has-test-dir current-dir)) (is-end-leaf-dir current-dir))
-    ;; Walk one directory up:
-    (setq current-dir (get-parent-dir current-dir))
-    )
-  (if (has-test-dir current-dir)
-      (get-create-test-file current-dir buffer-file-name) ;; then
-    (message "No `tests/` directory found.")              ;; else
-    )
-  )
-
-(global-set-key (kbd "C-c t") 'open-test-file)
-(global-set-key (kbd "C-c c") 'comment-or-uncomment-region)
