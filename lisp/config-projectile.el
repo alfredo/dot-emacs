b0dc4103225905bfd964521e0d4d130045044406
Alfredo Aguirre <alfredo@madewithbyt.es>
2018-08-23 19:22:17 +0200

    Update alias for projectile mode.

--- lisp/config-projectile.el
+++ lisp/config-projectile.el
@@ -39,6 +39,11 @@
         ".stack-work"
         "migrations"
         ))
+
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+
 (projectile-global-mode)
 (helm-projectile-on)
 
